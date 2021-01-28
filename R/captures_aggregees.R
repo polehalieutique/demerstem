8ce51c92a24d90de0a0ba54e69d0fe25d9dd8b57
jerome.guitton@agrocampus-ouest.fr <jerome.guitton@agrocampus-ouest.fr>
2021-01-28 18:52:20 +0100

    Ajout d'un jeux de données captures totales

---
+++ R/captures_aggregees.R
@@ -0,0 +1,12 @@
+#' Tableau de capture et effort de la pêche industrielle / Guinée
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{code_pays}{EEZ}
+#'   \item{annee}{Année}
+#'   \item{espece}{Nom de l'espèce capturée}
+#'   \item{capture}{Capture aggrégée}
+#'   ...
+#' }
+#' @source \url{CNSHB}
+"captures_aggregees"
