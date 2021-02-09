931316cdf81fae6e8d420309bb40997694b2d8af
Geti1234 <fabien.bourinet@gmail.com>
2021-02-09 16:03:43 +0100

    COMMENTAIRE : plusieurs modif ici, pour le coté pratiques 1) on enlève l'argument "logtrans" : inutile, puisqu'on transforme forcément en log (cf nature des data) 2) on print le glm à la fin, pour plus de clareté sur le modèle sélectionné (à peaufiner peut etre, en sautant des lignes, etc...?) 3) nouvelle argument "formule_select". Soit "auto", la fonction cherche comme avant la meilleure opti statistique (AIC). Soit on lui donne la formule que l'on souhaite tester, pour qu'il nous retounre graphs correspondant + info à son sujet. Sert à la fin, pour finaliser le modèle. 4) 2 modif envisagés : cf RQ1 et RQ2 fichier txt

--- R/glm_ia.R
+++ R/glm_ia.R
@@ -1,4 +1,4 @@
-#' la fonction glm_ia établit le modèle d'indices d'abondances : (sous-modèle positif dans un GLM delta). Une selection des parametre est effectuee sur critere statistique (stepAIC)
+#' la fonction glm_ia établit le modèle d'indices d'abondances : (sous-modèle positif dans un GLM delta). Une selection des parametre est effectuee sur critere statistique (stepAIC) ; ajout de l'argument "formule_select" : soit "auto" et la fonction choisit le modèle par opti stat, soit on lui donne la formule que l'on souhaite tester. Permet de finaliser.
 
 #' @param tab          : les données auxquelles ajuster le modèle
 #' @param parametres   : liste des parametres à tester
@@ -18,8 +18,7 @@
 #' @export
 
 
-
-glm_ia <- function(tab, parametres, logtrans, interactions){
+glm_ia <- function(tab, parametres, interactions, formule_select){
   #passer les parametres en facteur
   lapply(tab[,parametres], as.factor)
 
@@ -33,73 +32,15 @@ glm_ia <- function(tab, parametres, logtrans, interactions){
   formule_inter <- paste("i_ab ~ (", a, ")^2") #test des interactions ordre2
   formule_log_inter <- paste("log(i_ab+0.0001) ~ (", a,")^2")
 
-  if(logtrans=="auto"){
-    log_trans <<-"no" #par defaut, tant qu'aucune logtransformation n'est effectuée
-
-    #Ecriture du modele de base
-    Model <- glm(as.formula(formule) , family=gaussian, data=tab)
-    Model_log <- glm(as.formula(formule_log) , family=gaussian, data=tab) # ou get(parametres)
-
-    # Caution !!
-    # AIC
-    # Comments
-    # The AIC criteria uses the Deviance.
-    # A gaussian deviance with log-transformed values (what is done in AIC(model))
-    # IS NOT equivalent to computing a LogNormal deviance with non-transformed values
-    AIC.log <- AIC(Model_log)+2*sum(log(tab$i_ab), na.rm=TRUE)
-
-    if(	AIC.log < (AIC(Model)-10)){
-      Model_old <- Model
-      Model <- Model_log
-      formule <- formule_log
-      print(paste("les donnees sont passées au log (log+0.0001), l'AIC est amelioré de", AIC(Model_old)-AIC(Model_log), "points"))
-      log_trans <<- "yes"
-    }
-
-    if(interactions=="auto"){
-      #Ecriture du modele avec intercations sous conditions de nb_lignes^nb_parametres < 100000^5
-      if(nrow(tab)^length(parametres) < 10^25){
-        Model_inter <- glm(as.formula(formule_inter) , family=gaussian, data=tab)
-        Model_log_inter <- glm(as.formula(formule_log_inter) , family=gaussian, data=tab) # ou get(parametres)
-
-        AIC.log.inter <- AIC(Model_log_inter)+2*sum(log(tab$i_ab), na.rm=TRUE)
-
-        if(AIC(Model_inter) <(AIC(Model)-10)){
-          Model_old <- Model
-          Model <- Model_inter
-          formule <- formule_inter
-          print(paste("les interactions sont prises en compte, l'AIC est amelioré de", AIC(Model_old)-AIC(Model_inter), "points"))
-        }
-
-        if(AIC.log.inter < (AIC(Model)-10)){
-          Model_old <- Model
-          Model <- Model_log_inter
-          formule <- formule_log_inter
-          log_trans <<- "yes"
-          print(paste("les donnees sont passees au log (log+0.0001) et les interactions ordre 2 sont prises en compte, l'AIC est amelioré de", AIC(Model_old)-AIC(Model_log_inter), "points"))}
-      }else{
-        print("les interactions ordre 2 ne sont pas testees car le nombre de donnees ou de parametres est trop important")}
-
-    }else if(interactions=="force"){
-      Model_inter <- glm(as.formula(formule_inter) , family=gaussian, data=tab)
-      print(paste("les interactions sont prises en compte, l'AIC est amelioré de", AIC(Model)-AIC(Model_inter), "points"))
-      Model <- Model_inter
-      Model_log_inter <- glm(as.formula(formule_log_inter) , family=gaussian, data=tab) # ou get(parametres)
-      AIC.log.inter <- AIC(Model_log_inter)+2*sum(log(tab$i_ab))
-
-      if(AIC.log.inter <(AIC(Model)-10)){
-        Model <- Model_log_inter
-        formule <- formule_log_inter
-        print(paste("les donnees sont passees au log (log+0.0001) et les interactions ordre 2 sont prises en compte"))
-        log_trans <<- "yes"}
-    }
-  }else if (logtrans=="force") {
-    log_trans <<- "yes"
+  if (formule_select == "auto"){
     print("les donnees sont passées au log (log+0.0001)")
     #Ecriture du modele de base
     Model_log <- glm(as.formula(formule_log) , family=gaussian, data=tab) # ou get(parametres)
     AIC.log <- AIC(Model_log)+2*sum(log(tab$i_ab))
     Model <- Model_log
+    formule <- formule_log
+    print("Modele Initiale :")
+    print(formule)
 
 
     if(interactions=="auto"){
@@ -107,8 +48,8 @@ glm_ia <- function(tab, parametres, logtrans, interactions){
       if(nrow(tab)^length(parametres) < 100000^5){
         Model_log_inter <- glm(as.formula(formule_log_inter), family=gaussian, data=tab) # ou get(parametres)
 
-        AIC.log.inter <- AIC(Model_log_inter)+2*sum(log(tab$i_ab))
-        if(AIC(Model.log.inter)<(AIC(Model)-10)){
+        AIC_log_inter <- AIC(Model_log_inter)+2*sum(log(tab$i_ab))
+        if(AIC(Model_log_inter)<(AIC(Model)-10)){
           Model_old <- Model
           Model <- Model_log_inter
           formule <- formule_log_inter
@@ -120,22 +61,38 @@ glm_ia <- function(tab, parametres, logtrans, interactions){
       Model_log_inter <- glm(as.formula(formule_log_inter) , family=gaussian, data=tab) # ou get(parametres)
       AIC.log.inter <- AIC(Model_log_inter)+2*sum(log(tab$i_ab))
 
-      if(AIC(Model.log.inter)<(AIC(Model)-10)){
+      if(AIC(Model_log_inter)<(AIC(Model)-10)){
         Model <- Model_log_inter
         formule <- formule_log_inter
         print(paste("les donnees sont passees au log (log+0.0001) et les interactions ordre 2 sont prises en compte"))}
     }
+
+    #selection AIC
+    Model_aic=stepAIC(Model, formule ,trace=TRUE, direction="both")
+
+    if(AIC(Model_aic)<(AIC(Model)-5)){
+      Model_old <- Model
+      Model <- Model_aic
+      formule <- as.character(Model_aic$formula)[2]
+      print(paste("AIC ameliore de ", AIC(Model_old)-AIC(Model_aic), "points apres StepAIC"))
+    }else{
+      print("La selection stepAIC n'apporte pas d'e difference d'amelioration nette, elle n'est pas prise en compte")}
+
+  } else {
+    formule <- formule_select
+    Model <- glm(as.formula(formule) , family=gaussian, data=tab)
   }
 
-  #selection AIC
-  Model_aic=stepAIC(Model, formule ,trace=TRUE, direction="both")
 
-  if(AIC(Model_aic)<(AIC(Model)-5)){
-    Model_old <- Model
-    Model <- Model_aic
-    print(paste("AIC ameliore de ", AIC(Model_old)-AIC(Model_aic), "points apres StepAIC"))
-  }else{
-    print("La selection stepAIC n'apporte pas d'e difference d'amelioration nette, elle n'est pas prise en compte")}
+  # Print du modele sélectionné
+  if (formule_select == "auto"){
+    print("Le modele sélectionné par optimisation statistique est le suivant :")
+  } else {
+    print("Le modele que vous avez sélectionné :")
+  }
+  print(formule)
+  print(summary(Model))
+  print(anova(Model))
 
 
   # resume modele et graphiques residus
@@ -148,6 +105,6 @@ glm_ia <- function(tab, parametres, logtrans, interactions){
   qqnorm(res1) ; qqline(res1)
   plot(Model, 4)
 
-  log_trans
   return(Model)
 }
+
