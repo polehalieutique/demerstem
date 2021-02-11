58c7189c58863d8f9b4385c41cdaa3a383402b45
Geti1234 <fabien.bourinet@gmail.com>
2021-02-11 15:51:21 +0100

    rajout de formule_select et des plots des estimateurs ; on enlève logtrans

--- R/delta_abondance.R
+++ R/delta_abondance.R
@@ -5,6 +5,7 @@
 #' @param   param_test        : liste des facteurs potentiels à tester dans le glm
 #' @param   type_donnee       : "scientifique" ou "commercial"
 #' @param   effort            : "auto" pour un choix automatique de la variable effort ou choix personnel ex "duree_peche",   "nombre_operation","nb_jour_peche", "nb_sorties"
+#' @param   formule_select    : permet la selection manuel de la formule à tester dans le glm / "auto" sinon
 
 #' @examples
 #'  #PA
@@ -14,7 +15,7 @@
 #' #SC
 #' delta_abondance(tableau_sc, "PSEUDOTOLITHUS ELONGATUS", list_param, "scientifique", "auto", titre="SC", list_param,  espece_id_list, var_eff_list, ope_id, col_capture, seuil=0.05)
 #' @export
-delta_abondance <- function(tab, esp, param_test, type_donnee, effort, titre, list_param,  espece_id_list, var_eff_list, ope_id, col_capture, logtrans, interactions, seuil){
+delta_abondance <- function(tab, esp, param_test, type_donnee, effort, titre, list_param,  espece_id_list, var_eff_list, ope_id, col_capture, interactions, seuil, formule_select){
   print("SOUS-MODELE ABONDANCE")
   tableau_pres <- indice_ab_pres (tab, type_donnee, effort, esp, list_param,  espece_id_list, var_eff_list, ope_id, col_capture, seuil)
   tableau_ab <- filter(tableau_pres, presence==1)
@@ -25,6 +26,25 @@ delta_abondance <- function(tab, esp, param_test, type_donnee, effort, titre, li
   print(evo_an(tableau_ab, titre))
   print(lapply(param, evo_facto, tab=as.data.frame(tableau_ab), titre))
 
-  glm_indice_ab <- glm_ia(tableau_ab, param, logtrans, interactions)
+  glm_indice_ab <- glm_ia(tableau_ab, param, interactions, formule_select)
+
+  #Nouvelle partie
+
+  VAR <- var(residuals(glm_indice_ab))
+  table_ab <- as.data.frame(coef(summary(glm_indice_ab)))
+  table_ab$facteur <- as.factor(rownames(table_ab))
+  table_ab$ExpEstimate <- exp(table_ab$Estimate + 0.5*VAR) #correction de Laurent
+  table_ab <- table_ab %>% dplyr::select(-"t value")
+
+  for (i in 1:length(param)){
+    CODE <- substr(param[i], start = 1, stop = 3)
+    table_graph_estim <- table_ab %>% filter(substr(facteur, start = 1, stop = 3) == CODE)
+    if (nrow(table_graph_estim) > 0){
+      table_graph_estim <- table_graph_estim %>% mutate(variable = as.factor(substr(facteur, start = 1, stop =60)))
+      print(ggplot(table_graph_estim) + geom_bar(aes(x=variable, y=ExpEstimate), stat="identity", color = "black", fill = "white") + ylab("Estimateur") + ggtitle(paste(param[i], "pour abondance")) + theme(axis.text.x = element_text(angle = 35)))
+    }
+  }
+
   return(glm_indice_ab)
 }
+
