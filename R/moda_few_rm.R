68a26ef44bcb094afb8a6ea3a33ba905b770b63d
Geti1234 <fabien.bourinet@gmail.com>
2021-03-02 16:57:37 +0100

    Y a un pb dans le package. Je cherches ce que c'est au hasard

--- R/moda_few_rm.R
+++ R/moda_few_rm.R
@@ -1,7 +1,8 @@
-#'  nombre de donnees par modalite de chaque parametre
-#' @param tab tableau de données
-#' @param facteur_list facteurs
-#' @param seuil seuil
+#' nombre de donnees par modalite de chaque parametre
+
+#' @param tab : tableau de données
+#' @param facteur_list : facteurs
+#' @param seuil : seuil
 
 #' @examples
 #' tabz <- moda_few_rm(tableau_ab, names_facteur, 0.05)
@@ -13,3 +14,4 @@ moda_few_rm <- function(tab, factor_list, seuil){
   DT_keep = Reduce(intersect, DTs)
   return(DT_keep)
 }
+
