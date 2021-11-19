ed49905f0896c1c69fae8b42fdc7a0416403d06f
florianquemper <florian.quemper@agrocampus-ouest.fr>
2021-11-19 17:52:16 +0100

    fd

--- R/weight_size.R
+++ R/weight_size.R
@@ -8,8 +8,6 @@
 #' @examples
 #'  t <- 1
 #'  p <- c(0.005,3.16)
-#'  model_ai_plus(tableau_sc, esp = "PSEUDOTOLITHUS ELONGATUS", effort = "auto", title = "SC", list_param = c("annee", "saison", "strate"), var_eff_list=c("surface_chalutee"), espece_id='nom_taxonomique', catch_col='total_capture', interactions =FALSE, limit=0.001, formula_select = "log(i_ab+0.0001) ~ strate + annee + saison")
-#'
 #' @export
 
 weight_size <- function(t,p)
