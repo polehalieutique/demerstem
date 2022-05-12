c05a7ef2a17343a9d41067a1b8ade9f242fda4de
florianquemper <florian.quemper@agrocampus-ouest.fr>
2022-05-12 16:26:47 +0200

    log

--- R/fox_model.R
+++ R/fox_model.R
@@ -19,7 +19,7 @@ fox_model <- function(table_Efox, graph_param, a_start = 5, b_start= 3, log=TRUE
   start_a <- (upper_a + lower_a)/2
   upper_b <- max(table_Efox$Efox, na.rm=T) * b_start
 
-  if (logarithmic==TRUE){
+  if (log==TRUE){
     modelefox_IA <- nls(formula = log(IA) ~ log(a)-b*Efox, data=table_Efox, start = c(a = start_a , b = upper_b/2), algorithm = "port", lower = c(a = lower_a, b = 0), upper = c(a=upper_a, b = upper_b))
   } else {
     modelefox_IA <- nls(formula = IA ~ a*exp(-b*Efox), data=table_Efox, start = c(a = start_a , b = upper_b/2))
