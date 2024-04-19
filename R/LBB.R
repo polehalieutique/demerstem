c688b54188624637548a68953a4c03c34a43c69e
florianquemper <florian.quemper@agrocampus-ouest.fr>
2024-04-19 11:50:25 +0200

    PlotLBB.ts error

--- R/LBB.R
+++ R/LBB.R
@@ -1338,12 +1338,9 @@ plotLBB <- function(lfq, GausSel = FALSE, LcUser = NA, mmUser = FALSE, MKUser=NA
 #'
 #' @export
 #'
-plotLBB.ts <- function(lfq, mmUser = FALSE, GausSel = FALSE){
+plotLBB.ts <- function(res, mmUser = FALSE, GausSel = FALSE){
 
-  res <- lfq
-  res$refLev * 2 # because of en error
   Ldat <- res$refLev
-  Ldat2 <- Ldat #because of on error
 
   years <- as.numeric(format(res$dates, "%Y"))
 
